da61b6a909eb8281c412f3a2ecfc3c083120ff51
cs-48 <85981551+cs-48@users.noreply.github.com>
2024-01-21 20:45:35 +0530

    My Firrst commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_to_do_appliction
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_to_do_appliction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_to_do_appliction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
